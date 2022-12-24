95355fc97caa7c4780d53f38681dcbe52da76b26
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 20:52:36 +0100

    commit zero

---
+++ Horrific Housing Script pastebin roblox Horrific Housing Inf Money .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+game.ReplicatedStorage.ShopPurchase:FireServer(-10000, "Ornament")
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							You can get infinite money with Horrific Housing
+						
+]]
